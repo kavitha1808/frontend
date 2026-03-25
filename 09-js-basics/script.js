bb55ee1fb9de913e8887efd1a21a27c5a7b2ff37
Kavitha G <144310452+kavitha1808@users.noreply.github.com>
2026-03-25 23:06:58 +0530

    Create script.js

---
+++ 09-js-basics/script.js
@@ -0,0 +1,30 @@
+// DAY 9 - JavaScript Basics
+
+// 1. Variables
+let name = "Kavitha";
+let age = 20;
+const college = "Panimalar Engineering College";
+
+// 2. Print values using console.log()
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("College:", college);
+
+// 3. Data types
+let isStudent = true;
+let cgpa = 8.5;
+let favoriteLanguage = "JavaScript";
+
+console.log("Is Student:", isStudent);
+console.log("CGPA:", cgpa);
+console.log("Favorite Language:", favoriteLanguage);
+
+// 4. Add two numbers
+let num1 = 15;
+let num2 = 25;
+let sum = num1 + num2;
+
+// 5. Print result
+console.log("First Number:", num1);
+console.log("Second Number:", num2);
+console.log("Sum:", sum);
